b4f403dec3cbc3ce9cbdd42a5e42b5b0be2d3b4d
machmaegal <aka.pleat371@passinbox.com>
2025-01-08 17:43:27 +0100

    Completed lab

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,36 @@
 // src/main.ts
+import { getLocation, getCurrentWeather, displayLocation, displayWeatherData } from './utils'
+
+const weatherForm = document.querySelector('#weather-form') as HTMLElement
+
+weatherForm.addEventListener('submit', (e: Event) => {
+    e.preventDefault()
+
+    const locationInput = document.querySelector('#location') as HTMLInputElement
+
+    console.log(`The user has submitted the form 
+        and is searching for a location with this name ${locationInput.value}`)
+
+
+    getLocation(locationInput.value)
+        .then((response) => {
+            if (response.results) {
+                const location = response.results[0]
+
+                displayLocation(location)
+
+                return getCurrentWeather(location)
+            } else {
+                throw new Error('Location not found')
+            }
+        })
+        .then((weatherData) => {
+            displayWeatherData(weatherData)
+
+        })
+        .catch((error) => {
+            console.log("Error getting weather data")
+            console.log(error)
+        });
+    locationInput.value = ''
+})
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,34 @@
 // src/utils.ts
-
 import axios from 'axios';
-import { LocationResponse, Location } from "./types";
+import { LocationResponse, Location, WeatherResponse } from "./types";
 
+export async function getLocation(locationName: string): Promise<LocationResponse> {
 
-
-export function getLocation(locationName: string): Promise<LocationResponse> {
     const url = `https://geocoding-api.open-meteo.com/v1/search?name=${locationName}&count=1`;
-    return axios.get(url).then((response) => response.data);
+    return await axios.get(url).then((response) => response.data);
 }
 
+export async function getCurrentWeather(locationDetails: Location): Promise<WeatherResponse> {
 
+    return await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${locationDetails.latitude}&longitude=${locationDetails.longitude}&current_weather=true&models=icon_global`)
+        .then((res) => res.data)
+}
 
+export function displayLocation(locationDetails: Location): void {
+    const locationName = document.querySelector('#location-name') as HTMLElement
+    locationName.innerText = locationDetails.name
+
+    const country = document.querySelector('#country') as HTMLElement
+    country.innerText = locationDetails.country
+}
+
+export function displayWeatherData(obj: WeatherResponse): void {
+    const temp = document.querySelector('#temperature') as HTMLElement
+    temp.innerText = `${obj.current_weather.temperature} ${obj.current_weather_units.temperature}`
+
+    const windspeed = document.querySelector('#windspeed') as HTMLElement
+    windspeed.innerText = `${obj.current_weather.windspeed} ${obj.current_weather_units.windspeed}`
+
+    const winddirection = document.querySelector('#winddirection') as HTMLElement
+    winddirection.innerText = `${obj.current_weather.winddirection} ${obj.current_weather_units.winddirection}`
+}
